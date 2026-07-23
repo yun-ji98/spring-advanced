f0f809528a5f7687db6018dcc02ac0ef7b6e48a9
yun-ji98 <lyj77731@naver.com>
2026-07-23 17:16:48 +0900

    Lv1 해결: WebMvcConfig 추가

---
+++ src/main/java/org/example/expert/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package org.example.expert.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    public WebMvcConfig(AuthUserArgumentResolver authUserArgumentResolver) {
+        this.authUserArgumentResolver = authUserArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
+    }
+
+}
