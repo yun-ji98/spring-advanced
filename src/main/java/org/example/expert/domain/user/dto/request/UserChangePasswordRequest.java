465cfb1452b57392c6c0ef673247df7d3e94a8ed
yun-ji98 <lyj77731@naver.com>
2026-07-24 12:00:39 +0900

    Lv2(3) 해결: UserController, DTO 수정

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,10 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp = ".*\\d.*")
+    @Pattern(regexp = ".*[A-Z].*")
     private String newPassword;
 }
